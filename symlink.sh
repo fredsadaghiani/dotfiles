137044340a8edcd1320aa71ca1f84d605952efd1
Fred Sadaghiani <freds@agfachrome.local>
2014-03-04 19:26:15 -0800

    Initial commit

---
+++ symlink.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Symlinks dotfiles into home directory
+
+dir=`pwd`
+
+for file in .* bin; do
+    if [[ "$file" == ".git" || "$file" == "." || "$file" == ".." ]]; then
+        continue
+    fi
+    source="$dir/$file"
+    dest="$HOME/$file"
+    if [ -e "$dest" ]; then
+        mv "$dest" "$dest".orig
+    fi
+    ln -sfvn "$source" "$dest"
+done
