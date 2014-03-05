30e08ce555360ce2a37cf7cddacf8ac8e8e73e0f
Fred Sadaghiani <fred.sadaghiani@gmail.com>
2014-03-04 19:51:04 -0800

    Make symlink.sh script more robust

--- symlink.sh
+++ symlink.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-
 # Symlinks dotfiles into home directory
 
 dir=`pwd`
-
+if [[ $dir != "${HOME}/dotfiles" ]]; then
+  echo "Run this from ~/dotfiles, please" >&2
+  exit 1
+fi
 for file in .* bin; do
     if [[ "$file" == ".git" || "$file" == "." || "$file" == ".." ]]; then
         continue
@@ -11,7 +13,13 @@ for file in .* bin; do
     source="$dir/$file"
     dest="$HOME/$file"
     if [ -e "$dest" ]; then
-        mv "$dest" "$dest".orig
+        # And the file isn't already a symlink to our expected file
+        if [ ! -h "$dest" ]; then
+            mv "$dest" "$dest".orig
+        else
+            echo "$dest already symlinked"
+            continue
+        fi
     fi
     ln -sfvn "$source" "$dest"
 done
