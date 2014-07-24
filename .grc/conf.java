d0094e7e01386261aede9335e03767f51eca2e27
Fred Sadaghiani <fred.sadaghiani@gmail.com>
2014-07-24 11:13:37 -0700

    Add a default .irbrc file

---
+++ .grc/conf.java
@@ -0,0 +1,21 @@
+# this configuration file is suitable for displaying Maven output
+
+# colorize the DEBUG messages
+regexp=^.*DEBUG.*$
+colours="\033[38;5;153m"
+count=stop
+======
+# colorize the INFO messages
+regexp=^.*INFO.*$
+colours="\033[38;5;84m"
+count=stop
+======
+# colorize the WARN messages
+regexp=^.*WARN.*$
+colours="\033[38;5;222m"
+count=stop
+======
+# colorize the ERROR messages
+regexp=^.*ERROR.*$
+colours="\033[38;5;196m"
+count=stop
